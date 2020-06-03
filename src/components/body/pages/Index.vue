452d0373c3b737881f22b0b82fb0d4310719a723
skgoodo <1738550670@qq.com>
2020-06-03 22:20:37 +0800

    第一次修改

---
+++ src/components/body/pages/Index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <el-container>
+      <el-aside width="200px">
+        <Aside></Aside>
+      </el-aside>
+      <el-container>
+        <el-header>
+          <Header></Header>
+        </el-header>
+        <el-main>
+          <Breadcrumb></Breadcrumb>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+<script>
+import Header from "@/components/body/pages/Header.vue";
+import Aside from "@/components/body/pages/Aside.vue";
+import Breadcrumb from "@/components/body/pages/Breadcrumb.vue";
+export default {
+  components: {
+    Header,
+    Aside,
+    Breadcrumb
+  }
+};
+</script>
+<style>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,24 +1,17 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
-
+import Login from "@/components/body/Login";
+import Home from "@/components/body/pages/Index.vue";
 Vue.use(Router);
-
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: "/login",
+      component: Login
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/home",
+      component: Home
     }
   ]
 });
